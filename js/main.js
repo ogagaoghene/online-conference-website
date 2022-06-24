bfcbd05f7d8b54168149410f45c3633179a86ec6
Esi Ogagaoghene <esi.ogagaoghene@yahoo.com>
2022-06-24 18:23:26 +0100

    Update main.js file by fixing the hamburger function

--- js/main.js
+++ js/main.js
@@ -128,22 +128,19 @@ loadFeaturedSpeakers();
 
 const hamburger = document.querySelector('.hamburger');
 const navMenu = document.querySelector('.nav-menu');
-const body = document.querySelector('.main-section');
 
 const mobileMenu = () => {
   hamburger.classList.toggle('active');
   if (hamburger.classList.contains('active')) {
     hamburger.innerHTML = '<i class="fas fa-times"  style="color:#fff"></i>';
-  } else { hamburger.innerHTML = '<i class="fas fa-bars"></i>'; }
+  } else { hamburger.innerHTML = '<i class="fas fa-bars style="color:#fff"></i>'; }
   navMenu.classList.toggle('active');
-  body.forEach((n) => n.classList.toggle('bgcolor'));
 };
 
 const closeMenu = () => {
-  hamburger.innerHTML = "<i class='fas fa-bars'></i>";
+  hamburger.innerHTML = "<i class='fas fa-bars' ></i>";
   navMenu.classList.remove('active');
   hamburger.classList.remove('active');
-  body.forEach((n) => n.classList.remove('bgcolor'));
 };
 
 const navLink = document.querySelectorAll('.nav-link');
